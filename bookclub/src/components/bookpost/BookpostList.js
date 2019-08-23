c8f5de2722f565b4a65a3f68bfbe7f9284916d5d
George Wan <singonwan@gmail.com>
2019-08-23 11:35:35 -0400

    nested bookpostsummary into bookpostlist for modular design

--- bookclub/src/components/bookpost/BookpostList.js
+++ bookclub/src/components/bookpost/BookpostList.js
@@ -1,33 +1,12 @@
 import React from 'react'
+import BookpostSummary from './BookpostSummary'
 
 const BookpostList = () => {
     return (
         <div className="bookpost-list section">
-
-            <div className="card z-depth-0 bookpost-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="class-title">Bookpost Title</span>
-                    <p>Posted by GWAN</p>
-                    <p className="grey-text">23 august</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 bookpost-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="class-title">Bookpost Title</span>
-                    <p>Posted by GWAN</p>
-                    <p className="grey-text">23 august</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 bookpost-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="class-title">Bookpost Title</span>
-                    <p>Posted by GWAN</p>
-                    <p className="grey-text">23 august</p>
-                </div>
-            </div>
-            
+            <BookpostSummary />
+            <BookpostSummary />
+            <BookpostSummary />
         </div>
     )
 }
