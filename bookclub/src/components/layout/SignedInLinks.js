2aaa78e449035989fb05e3d2bb8bdfcc124b1a06
George Wan <singonwan@gmail.com>
2019-08-23 12:59:38 -0400

    linked up the navbar with navlinks and changed the css and added background to make site nicer

--- bookclub/src/components/layout/SignedInLinks.js
+++ bookclub/src/components/layout/SignedInLinks.js
@@ -3,8 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 const SignedInLinks = () => {
     return (
+        //initials can go to profile page later. -number of books, fave book, etc
         <ul className="right">
-            <li><NavLink to='/'>New BookPost</NavLink></li>
+            <li><NavLink to='/create'>New BookPost</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating grey'>GW</NavLink></li>
         </ul>
