5bd35e5002f9683b98be55c5d3e135e6fedd530e
George Wan <singonwan@gmail.com>
2019-08-23 11:31:19 -0400

    created dashboard components rough

---
+++ bookclub/src/components/bookpost/BookpostList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const BookpostList = () => {
+    return (
+        <div className="bookpost-list section">
+
+            <div className="card z-depth-0 bookpost-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Bookpost Title</span>
+                    <p>Posted by GWAN</p>
+                    <p className="grey-text">23 august</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 bookpost-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Bookpost Title</span>
+                    <p>Posted by GWAN</p>
+                    <p className="grey-text">23 august</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 bookpost-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="class-title">Bookpost Title</span>
+                    <p>Posted by GWAN</p>
+                    <p className="grey-text">23 august</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default BookpostList
