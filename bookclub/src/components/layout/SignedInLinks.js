e19d714960d0b78c4af173d8c28d119edb203df4
George Wan <singonwan@gmail.com>
2019-08-28 16:17:16 -0400

    logging users out

--- bookclub/src/components/layout/SignedInLinks.js
+++ bookclub/src/components/layout/SignedInLinks.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
     return (
         //initials can go to profile page later. -number of books, fave book, etc
         <ul className="right">
             <li><NavLink to='/create'>New BookPost</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
+            <li><a onClick={props.signOut}>Log Out</a></li>
             <li><NavLink to='/' className='btn btn-floating grey'>GW</NavLink></li>
         </ul>
     )
 }
 
-export default SignedInLinks
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+export default connect(null, mapDispatchToProps)(SignedInLinks)
--- bookclub/src/store/reducers/authReducer.js
+++ bookclub/src/store/reducers/authReducer.js
@@ -13,7 +13,10 @@ const authReducer = (state = initState, action) => {
             return {
                 ...state,
                 authError: null
-            }
+            } 
+        case 'SIGNOUT_SUCCESS':
+            console.log("signout success")
+            return state;
         default:
             return state;
     }
