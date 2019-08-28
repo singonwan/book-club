5d9109bd34a56a426cd5da40968a1d2aa7bb52fb
George Wan <singonwan@gmail.com>
2019-08-28 15:16:53 -0400

    syncing data with project details page

--- bookclub/src/components/bookpost/BookpostDetail.js
+++ bookclub/src/components/bookpost/BookpostDetail.js
@@ -1,21 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const BookpostDetail = (props) => {
-    const id = props.match.params.id;
-    return (   
-        <div className="container section bookpost-detail">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Bookpost Title - id : {id}</span>
-                    <p>blah blah blah</p>
-                </div>
-                <div className="card-action grey lighten-4 brown-text">
-                    <div>Posted by Gwanolds</div>
-                    <div>23 august</div>
+    const { bookpost } = props;
+    if (bookpost) {
+        return (
+            <div className="container section bookpost-detail">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{ bookpost.title }</span>
+                        <p>{ bookpost.content }</p>
+                    </div>
+                    <div className="card-action grey lighten-4 brown-text">
+                        <div>Posted by { bookpost.authorFirstName } {bookpost.authorLastName }</div>
+                        <div>23 august</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (   
+            <div className="container center">
+                <p>Loading Bookpost...</p>
+            </div>
+        )
+    }
 }
 
-export default BookpostDetail
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const bookposts = state.firestore.data.bookposts;
+    const bookpost = bookposts ? bookposts[id] : null
+    return {
+        bookpost: bookpost //now BookpostDetail will have a prop called bookpost!
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'bookposts' }
+    ])
+)(BookpostDetail)
--- bookclub/src/components/bookpost/BookpostList.js
+++ bookclub/src/components/bookpost/BookpostList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import BookpostSummary from './BookpostSummary'
+import { Link } from 'react-router-dom'
 
 const BookpostList = ({bookposts}) => {
     return (
         <div className="bookpost-list section">
             { bookposts && bookposts.map(bookpost => {
                 return(
-                    <BookpostSummary bookpost={bookpost} key={bookpost.id}/>
+                    <Link to={'/bookpost/' + bookpost.id}>
+                        <BookpostSummary bookpost={bookpost} key={bookpost.id}/>
+                    </Link>
                 )
             })}
         </div>
